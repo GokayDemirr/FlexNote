407461dc1d37edf8854081c5f855689218b1d836
GokayDemirr <gokaydemirpc@gmail.com>
2025-02-19 02:41:07 +0300

    UPDATE types for sidebarCategory data

--- src/data/sidebarCategories.tsx
+++ src/data/sidebarCategories.tsx
@@ -1,25 +1,53 @@
 import BookIcon from "@mui/icons-material/Book";
-import ArticleIcon from "@mui/icons-material/Article";
+// import ArticleIcon from "@mui/icons-material/Article";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
 import { ReactNode } from "react";
 
-interface SidebarCategory {
+export interface SidebarCategory {
   id: number;
   name: string;
-  amount: number;
+  notebookAmount: number;
   icon: ReactNode;
+  isHaveDropdown: boolean;
+  notebooks: [
+    {
+      id: number;
+      title: string;
+      noteAmount: number;
+      notes: [
+        {
+          id: number;
+          title: string;
+          content: string;
+        }
+      ];
+      icon: ReactNode;
+    }
+  ];
 }
 
 export const sidebarCategories: SidebarCategory[] = [
   {
     id: 1,
-    name: "All Notes",
-    amount: 10,
-    icon: <ArticleIcon />,
-  },
-  {
-    id: 2,
     name: "Notebooks",
-    amount: 5,
+    notebookAmount: 5,
     icon: <BookIcon />,
+    isHaveDropdown: true,
+    notebooks: [
+      {
+        id: 1,
+        title: "notebook1",
+        noteAmount: 1,
+        notes: [
+          {
+            id: 1,
+            title: "not1",
+            content: "notcontent1",
+          },
+        ],
+
+        icon: <ImportContactsIcon />,
+      },
+    ],
   },
 ];
